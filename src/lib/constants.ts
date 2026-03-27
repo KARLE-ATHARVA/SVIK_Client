9d615b4461db4ac177534fdef86b930ddd743f5c
Devishree Nadar <devishreenadar@gmail.com>
2026-03-27 17:11:34 +0530

    Fix API base env usage

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ export const CONCEPT_IMAGE_BASE_URL =
   process.env.NEXT_PUBLIC_CONCEPT_IMAGE_BASE_URL;
 
 //export const API_BASE = "http://localhost:5109";
-export const API_BASE =process.env.NEXT_PUBLIC_API_BASE2; 
+export const API_BASE = process.env.NEXT_PUBLIC_API_BASE;
 
 export const ASSET_BASE =
   process.env.NEXT_PUBLIC_ASSET_BASE;
